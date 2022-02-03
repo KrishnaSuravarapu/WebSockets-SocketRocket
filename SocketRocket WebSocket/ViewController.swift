eac4b4a5028e221888a913530e3e4b3d48d6e9d6
KrishnaSuravarapu <krishna.su@browserstack.com>
2022-02-03 23:24:29 +0530

    initial commit

--- SocketRocket WebSocket/ViewController.swift
+++ SocketRocket WebSocket/ViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import SocketRocket
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SRWebSocketDelegate {
+    
+    var socket: SRWebSocket!
+    
+    var isConnected = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var request = URLRequest(url: URL(string: ProcessInfo.processInfo.arguments[1])!) //ProcessInfo.processInfo.arguments[1]
+        request.timeoutInterval = 5
+        socket = SRWebSocket(urlRequest: request)
+        socket.delegate = self
+    }
+    
+    func displayAlert(alertMessage: String) {
+        let alert = UIAlertController(title: "Response", message: alertMessage, preferredStyle: UIAlertController.Style.alert)
+
+                // add an action (button)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+                // show the alert
+                self.present(alert, animated: true, completion: nil)
+    }
+    
+    func webSocketDidOpen(_ webSocket: SRWebSocket) {
+        self.displayAlert(alertMessage: "Connected")
+        isConnected = true
+    }
+    
+    func webSocket(_ webSocket: SRWebSocket, didCloseWithCode code: Int, reason: String?, wasClean: Bool) {
+        self.displayAlert(alertMessage: "Disconnected")
+        isConnected = false
+    }
+    
+    func webSocket(_ webSocket: SRWebSocket!, didReceiveMessage message: Any!){
+        self.displayAlert(alertMessage: message as! String)
     }
 
 
+    @IBAction func connectToggle(_ sender: UIBarButtonItem) {
+        if isConnected {
+            sender.title = "Connect"
+            socket.close()
+        } else {
+            sender.title = "Disconnect"
+            socket.open()
+        }
+    }
+    
+    @IBAction func sendMessage(_ sender: UIBarButtonItem) {
+        do {
+            try socket.send(string: "Request")
+        } catch let error{
+            print(error)
+        }
+    }
 }
 
